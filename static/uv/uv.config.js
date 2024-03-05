efe6cdde4d396c9054cdc2bac54c1196cc585212
ubgultra <155318917+ubgultra@users.noreply.github.com>
2024-03-05 13:52:39 -0500

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/UUB-Plus-GH/static/tiw/',
-    bare:'https://ubgultra.github.io/UUB-Plus-GH/barething',
+    bare:'https://ubgultra.github.io/UUB-Plus-GH/barestuff',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: 'https://ubgultra.github.io/UUB-Plus-GH/static/uv/uv.handler.js',

cad07d1edd2333d4c48782febb63e3ac0968109f
ubgultra <155318917+ubgultra@users.noreply.github.com>
2024-03-05 08:35:44 -0500

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -3,8 +3,8 @@ self.__uv$config = {
     bare:'https://bare.benrogo.net',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: '/static/uv/uv.handler.js',
-    bundle: '/static/uv/uv.bundle.js',
-    config: '/static/uv/uv.config.js',
+    handler: 'https://ubgultra.github.io/UUB-Plus-GH/static/uv/uv.handler.js',
+    bundle: 'https://ubgultra.github.io/UUB-Plus-GH/static/uv/uv.bundle.js',
+    config: 'https://ubgultra.github.io/UUB-Plus-GH/static/uv/uv.config.js',
     sw: '/static/uv/uv.sw.js',
 };
